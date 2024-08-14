621ccbfe254eae202c82866aa80c4a35ed8ee879
Jahn1295 <jiheeahn1222@gmail.com>
2024-08-14 20:10:42 +0000

    working progess

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,70 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+import { createUser, fetchTodos, addTodoToAPI } from "../updateAPI.js";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+
+	useEffect(() => {
+		fetchTodos(setTodos);
+	}, [])
+
+	const handleAddTodo = (e) => {
+		if (e.key === "Enter" && inputValue.trim() !== "") {
+			const newTodo = {
+				id: Date.now(),
+				label: inputValue.trim(),
+				done: false
+			};
+			setTodos([...todos, newTodo]);
+
+			addTodoToAPI(todos, inputValue, setTodos);
+			console.log("New Todo added to API: " + newTodo.label)
+
+			setInputValue('');
+		}
+	}
+	const handleDeleteTodo = (index) => {
+		setTodos(todos.filter((todo, i) => index !== i))
+	}
+	const handleCreateUser = () => {
+		createUser();
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todoListBody">
+			<h1 className="title">My ToDo's List</h1>
+			<ul className="list-group">
+				<li className="list-group-item">
+					<input
+						type='text'
+						value={inputValue}
+						onKeyDown={handleAddTodo}
+						placeholder="What Needs to be Done?"
+						onChange={(e) => setInputValue(e.target.value)}
+					/>
+				</li>
+				{todos.length === 0 ? (
+					<li className="list-group-item">No tasks. Add a task.</li>
+
+				) : (
+					todos.map((todo, index) => (
+						<li className="list-group-item" key={todo.id}>
+							<div className="list-group-item-todo" id="screen">
+								{todo.label}
+							</div>
+							<span className="x-container" onClick={() => handleDeleteTodo(index)}><i className="fa-solid fa-trash-can"></i></span>
+						</li>
+					))
+				)}
+			</ul>
+			<div>
+				{todos.length} {todos.length === 1 ? "item" : "items"} left
+			</div>
+			<div className="btn-div">
+				<button onClick={handleCreateUser} className="btn btn-primary">Create User to save tasks</button>
+				{/* <button onClick={handleClearTasks} className="btn btn-primary">Clear User and tasks</button> */}
+			</div>
 		</div>
 	);
 };
---
+++ src/js/updateAPI.js
@@ -0,0 +1,69 @@
+export const createUser = () => {
+    fetch('https://playground.4geeks.com/todo/users/Jihee', {
+        method: "POST",
+        body: JSON.stringify([]),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((resp) => {
+            if (resp.ok) {
+                console.log("Users been successfully created in the API");
+                alert("You can now save tasks to the API");
+            }
+        })
+        .catch((error) => console.error("Error creating user in API:", error));
+};
+
+
+export const fetchTodos = (setTodos) => {
+    fetch('https://playground.4geeks.com/todo/users/Jihee')
+        .then((resp) => {
+            if (!resp.ok) {
+                throw new Error("Failed to fetch todo list. Status: " + resp.status);
+            }
+            return resp.json();
+        })
+        .then((data) => {
+            console.log("Todo list from API", data);
+            if (Array.isArray(data.todos)) {
+                setTodos(data.todos);
+            } else {
+                console.error("Fetched data is not an Array", data.todos);
+                setTodos([]);
+            }
+        
+        })
+        .catch((error) => console.error("There's been a problem with your fetch opp:", error));
+};
+
+export const addTodoToAPI = async (todos, inputValue, setTodos ) => {
+    try {
+        const newTodo = {
+            label: inputValue.trim(),
+            is_done: false
+        };
+        
+        const response = await fetch('https://playground.4geeks.com/todo/todos/Jihee', {
+            method: "POST",
+            body: JSON.stringify(newTodo),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch todo list. Status: " + resp.status);
+        }
+
+        const data = await response.json();
+        const updatedTodos = [
+            ...todos,
+            { ...newTodo, id: data.id},
+        ];
+        setTodos(updatedTodos);
+        fetchTodos(setTodos);
+    } catch (error) {
+        console.error("There's been a problem with your fetch opp:", error)
+    }
+}
